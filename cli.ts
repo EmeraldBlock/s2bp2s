124066766aa48dbf6bfa6fb6a1e6428d8ca792dd
EmeraldBlock <yygengjunior@gmail.com>
2023-05-06 13:25:31 -0500

    cli improvements

--- cli.ts
+++ cli.ts
@@ -1,31 +1,66 @@
 import * as path from "path";
 import * as fs from "fs/promises";
+
+import minimist from "minimist";
+
 import { render } from "./src/renderer.ts";
 import { deserialize } from "./src/serializer.ts";
 import { OUT_DIR } from "./src/config.ts";
 
-await fs.mkdir(OUT_DIR, { recursive: true });
+(async () => {
+    const args = minimist<{
+        help: boolean,
+        i?: string,
+        o?: string,
+    }>(process.argv.slice(2), {
+        string: ["i", "o"],
+        boolean: ["help"],
+    });
 
-function textLineStream(stream: NodeJS.ReadableStream) {
-    stream.setEncoding("utf-8");
-    return {
-        async *[Symbol.asyncIterator]() {
-            let buf = "";
-            for await (const chunk of stream) {
-                const lines = (chunk as string).split(/\r?\n/g);
-                lines[0] = buf + lines[0];
-                buf = lines.pop();
-                for (const line of lines) {
-                    yield line;
+    if (args.help) {
+        console.log(`\
+Options:
+    --help
+        Display this help message.
+    -i [<PATH>]
+        Read blueprint string from specified file instead of from standard input.
+    -o [<PATH>]
+        Write schematic image to specified file instead of to /out/image.png.`
+        );
+        return;
+    }
+
+    await fs.mkdir(OUT_DIR, { recursive: true });
+
+    function textLineStream(stream: NodeJS.ReadableStream) {
+        stream.setEncoding("utf-8");
+        return {
+            async *[Symbol.asyncIterator]() {
+                let buf = "";
+                for await (const chunk of stream) {
+                    const lines = (chunk as string).split(/\r?\n/g);
+                    lines[0] = buf + lines[0];
+                    buf = lines.pop();
+                    for (const line of lines) {
+                        yield line;
+                    }
                 }
-            }
-        },
-    };
-}
+            },
+        };
+    }
 
-process.stdout.write("Blueprint: ");
-/** @todo TextDecoderStream seems to cause it to wait for an extra line of interactive input? */
-for await (const line of textLineStream(process.stdin)) {
-    await fs.writeFile(path.join(OUT_DIR, "./image.png"), render(await deserialize(line.trim())));
-    break;
-}
+    const content = await (async () => {
+        if ("i" in args) {
+            return await fs.readFile(args.i, "utf-8");
+        } else {
+            process.stdout.write("Blueprint: ");
+            for await (const line of textLineStream(process.stdin)) {
+                return line;
+            }
+        }
+    })();
+
+    await fs.writeFile(args.o ?? path.join(OUT_DIR, "./image.png"), render(await deserialize(content.trim())));
+
+    console.log("Done.");
+})();
--- services/bot/deploy-commands.ts
+++ services/bot/deploy-commands.ts
@@ -2,6 +2,7 @@ import * as url from "url";
 
 import Discord from "discord.js";
 import chalk from "chalk";
+import minimist from "minimist";
 
 import { readdirSafe } from "./utils.ts";
 import { COMMANDS_DIR } from "./constants.ts";
@@ -10,37 +11,73 @@ import secrets from "./config/secrets.ts";
 
 import type { Command } from "./index.ts";
 
-const files = (await readdirSafe(COMMANDS_DIR)).filter(file => file.endsWith(".ts"));
+(async () => {
+    const args = minimist<{
+        help: boolean,
+        global: boolean,
+        guild?: string | string[],
+    }>(process.argv.slice(2), {
+        string: ["guild"],
+        boolean: ["help", "global"],
+    });
 
-const commands: Array<Discord.RESTPostAPIApplicationCommandsJSONBody> = [];
-const collection: Discord.Collection<string, Command> = new Discord.Collection();
-for (const file of files) {
-    const val = await import(url.pathToFileURL(`${COMMANDS_DIR}${file}`).href) as { default: Command };
-    const item = val.default;
-    const name = item.name;
-    item.data
-        .setName(name)
-        .setDescription(item.desc);
-    if (collection.has(name)) {
-        console.log(`Duplicate name or alias ${chalk.yellow(name)}.`);
-        continue;
+    if (args.help) {
+        console.log(`\
+Options:
+    --help
+        Display this help message.
+    --global
+        Deploy to global commands.
+    --guild
+        Deploy to configured dev guild's commands.
+        Default when none of --global or --guild[=<ID>] is present.
+    --guild=[<ID>]
+        Deploy to specific guild's commands. May be repeated.`
+        );
+        return;
     }
-    collection.set(name, item);
-    commands.push(item.data.toJSON());
-}
 
-const rest = new Discord.REST().setToken(secrets.token);
+    const files = (await readdirSafe(COMMANDS_DIR)).filter(file => file.endsWith(".ts"));
 
-try {
-    console.log("Starting to deploy commands.");
-    /** @todo support deploying global commands */
-    await rest.put(
-        Discord.Routes.applicationGuildCommands(secrets.clientId, secrets.devGuildId),
-        { body: commands },
-    );
-    console.log("Successfully deployed commands.")
-} catch (err) {
-    console.error("Failed to deploy commands.");
-    console.error(err);
-    process.exit(1);
-}
+    const commands: Array<Discord.RESTPostAPIApplicationCommandsJSONBody> = [];
+    const collection: Discord.Collection<string, Command> = new Discord.Collection();
+    for (const file of files) {
+        const val = await import(url.pathToFileURL(`${COMMANDS_DIR}${file}`).href) as { default: Command };
+        const item = val.default;
+        const name = item.name;
+        item.data
+            .setName(name)
+            .setDescription(item.desc);
+        if (collection.has(name)) {
+            console.log(`Duplicate name or alias ${chalk.yellow(name)}.`);
+            continue;
+        }
+        collection.set(name, item);
+        commands.push(item.data.toJSON());
+    }
+
+    const rest = new Discord.REST().setToken(secrets.token);
+
+    try {
+        console.log("Starting deploying commands.");
+        if (args.global) {
+            await rest.put(
+                Discord.Routes.applicationCommands(secrets.clientId),
+                { body: commands },
+            );
+            console.log("   Deployed to global.");
+        }
+        for (const id of [args.guild ?? ""].flat()) {
+            await rest.put(
+                Discord.Routes.applicationGuildCommands(secrets.clientId, id == "" ? secrets.devGuildId : id),
+                { body: commands },
+            );
+            console.log(`   Deployed to guild ${id == "" ? "dev" : id}.`);
+        }
+        console.log("Successfully deployed commands.")
+    } catch (err) {
+        console.error("Failed to deploy commands.");
+        console.error(err);
+        process.exit(1);
+    }
+})();
